7d68f4da13253293844885dc312c6854f327527e
wout.werkman <wout.werkman@jetbrains.com>
2022-06-30 19:11:22 +0200

    Initial Commit

---
+++ SshKeyTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SshKeyTest
+//
+//  Created by Wout Werkman on 30.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SshKeyTest/SshKeyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SshKeyTestApp.swift
+//  SshKeyTest
+//
+//  Created by Wout Werkman on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SshKeyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
